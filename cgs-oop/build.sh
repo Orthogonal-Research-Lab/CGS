6d032c10d21e36e557a2235169230f8f76e6d884
Sam Felder <samfelder17@gmail.com>
2018-05-24 12:19:44 -0500

    fixed build script

--- cgs-oop/build.sh
+++ cgs-oop/build.sh
@@ -1,2 +1,7 @@
-blender oop-blender-demo.blend -P model.py -- $1
-blender oop-blender-demo.blend -a
+#!/bin/bash
+
+
+blender=/Applications/Blender/blender.app/Contents/MacOS/blender
+
+$blender -b oop-blender-demo.blend -P model.py -- $1
+$blender -b oop-blender-demo.blend -a
