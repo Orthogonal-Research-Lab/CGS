569559d89c6ebd7ccb837369b4d7474943c1dffd
Sam Felder <samfelder17@gmail.com>
2018-06-14 22:12:41 -0500

    added optional arguments to create data and run knn

--- cgs-oop/create_data.py
+++ cgs-oop/create_data.py
@@ -2,33 +2,47 @@ import PIL.ImageDraw as ImageDraw,PIL.Image as Image, PIL.ImageShow as ImageShow
 import numpy
 import sys
 import pickle
+import argparse
 from skimage.feature import canny
 
-list_of_training_data = []
 
-for i in range(0,20):
-    im = Image.new("1", (20,20))
-    draw = ImageDraw.Draw(im)
-    for j in range(i, 20):
-        for x in range(i,20 - i):
-            for y in range(j,20 -j):
-                if i > 3 and j > 3:
-                    draw.rectangle(((i,j),(x+i,y+j)), fill= "white")
-                    pix = numpy.array(im)
-                    edges2 = canny(pix)
-                    list_of_training_data.append((edges2,'Rectangle'))
+def create_data(pixels):
 
-for i in range(0,20):
-    im = Image.new("1", (20,20))
-    draw = ImageDraw.Draw(im)
-    for j in range(i, 20):
-        for x in range(i,20 - i):
-            for y in range(j,20 -j):
-                if i > 3 and j > 3:
-                    draw.ellipse(((i,j),(x+i,y+j)), fill= "white")
-                    pix = numpy.array(im)
-                    edges2 = canny(pix)
-                    list_of_training_data.append((edges2,'Circle'))
+    list_of_training_data = []
 
-with open('data.pickle','wb') as f:
-    pickle.dump(list_of_training_data,f)
+    for i in range(0,pixels):
+        im = Image.new("1", (pixels,pixels))
+        draw = ImageDraw.Draw(im)
+        for j in range(i, pixels):
+            for x in range(i,pixels - i):
+                for y in range(j,pixels -j):
+                    if i > 3 and j > 3:
+                        draw.rectangle(((i,j),(x+i,y+j)), fill= "white")
+                        pix = numpy.array(im)
+                        edges2 = canny(pix)
+                        list_of_training_data.append((edges2,'Rectangle'))
+
+    for i in range(0,pixels):
+        im = Image.new("1", (pixels,pixels))
+        draw = ImageDraw.Draw(im)
+        for j in range(i, pixels):
+            for x in range(i,pixels - i):
+                for y in range(j,pixels -j):
+                    if i > 3 and j > 3:
+                        draw.ellipse(((i,j),(x+i,y+j)), fill= "white")
+                        pix = numpy.array(im)
+                        edges2 = canny(pix)
+                        list_of_training_data.append((edges2,'Circle'))
+
+    return list_of_training_data
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pixels', nargs='?', default=15)
+    arg = parser.parse_args()
+
+    dataset = create_data(arg.pixels)
+
+    with open('data.pickle','wb') as f:
+        pickle.dump(dataset,f)
