90d4f2baf4273854a0bb94a028e9a80c10037cd9
Sam Felder <samfelder17@gmail.com>
2018-06-14 21:18:59 -0500

    finished classifying between square and circle with accuracy of 90+ percent

--- cgs-oop/create_data.py
+++ cgs-oop/create_data.py
@@ -6,28 +6,29 @@ from skimage.feature import canny
 
 list_of_training_data = []
 
-for i in range(0,32):
-    im = Image.new("1", (32,32))
+for i in range(0,20):
+    im = Image.new("1", (20,20))
     draw = ImageDraw.Draw(im)
-    for j in range(i, 32):
-        for x in range(i,32 - i):
-            for y in range(j,32 -j):
-                draw.rectangle(((i,j),(x+i,y+j)), fill= "white")
-                pix = numpy.array(im)
-                edges2 = canny(pix)
-                list_of_training_data.append((edges2,'Rectangle'))
+    for j in range(i, 20):
+        for x in range(i,20 - i):
+            for y in range(j,20 -j):
+                if i > 3 and j > 3:
+                    draw.rectangle(((i,j),(x+i,y+j)), fill= "white")
+                    pix = numpy.array(im)
+                    edges2 = canny(pix)
+                    list_of_training_data.append((edges2,'Rectangle'))
 
-for i in range(0,32):
-    im = Image.new("1", (32,32))
+for i in range(0,20):
+    im = Image.new("1", (20,20))
     draw = ImageDraw.Draw(im)
-    for j in range(i, 32):
-        for x in range(i,32 - i):
-            for y in range(j,32 -j):
-                draw.ellipse(((i,j),(x+i,y+j)), fill= "white")
-                pix = numpy.array(im)
-                edges2 = canny(pix)
-                list_of_training_data.append((edges2,'Circle'))
+    for j in range(i, 20):
+        for x in range(i,20 - i):
+            for y in range(j,20 -j):
+                if i > 3 and j > 3:
+                    draw.ellipse(((i,j),(x+i,y+j)), fill= "white")
+                    pix = numpy.array(im)
+                    edges2 = canny(pix)
+                    list_of_training_data.append((edges2,'Circle'))
 
-filehandler = open('training.obj',"wb")
-pickle.dump(list_of_training_data,filehandler)
-filehandler.close()
+with open('data.pickle','wb') as f:
+    pickle.dump(list_of_training_data,f)
--- cgs-oop/toy_knn.py
+++ cgs-oop/toy_knn.py
@@ -6,83 +6,80 @@ import operator
 from skimage.feature import canny
 import PIL.ImageDraw as ImageDraw,PIL.Image as Image, PIL.ImageShow as ImageShow 
 import numpy
+import pickle
 import sys
 
 def loadDataset(filename, split, trainingSet=[] , testSet=[]):
-	with open(filename, 'rb') as csvfile:
-	    lines = csv.reader(csvfile)
-	    dataset = list(lines)
-	    for x in range(len(dataset)-1):
-	        for y in range(4):
-	            dataset[x][y] = float(dataset[x][y])
-	        if random.random() < split:
-	            trainingSet.append(dataset[x])
-	        else:
-	            testSet.append(dataset[x])
-
+    # with open(filename, 'rb') as csvfile:
+        # lines = csv.reader(csvfile)
+        # dataset = list(lines)
+    filed = open('data.pickle','rb')
+    list_of_training_data = pickle.load(filed)
+    for edges in list_of_training_data:
+        # for y in range(4):
+            # dataset[x][y] = float(dataset[x][y])
+        if random.random() < split:
+            trainingSet.append(edges)
+        else:
+            testSet.append(edges)
 
 def euclideanDistance(instance1, instance2, length):
-	distance = 0
-	for x in range(length):
-		distance += pow((instance1[x] - instance2[x]), 2)
-	return math.sqrt(distance)
+    distance = 1024
+    for x in range(length):
+        for y in range(length):
+            if(instance1[0][x,y]==instance2[0][x,y]):
+                distance -=1
+        # distance += pow((instance1[x] - instance2[x]), 2)
+# return math.sqrt(distance)
+    return distance
 
 def getNeighbors(trainingSet, testInstance, k):
-	distances = []
-	length = len(testInstance)-1
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(testInstance, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))
-	neighbors = []
-	for x in range(k):
-		neighbors.append(distances[x][0])
-	return neighbors
+    distances = []
+    length = len(testInstance[0])-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+            neighbors.append(distances[x][0])
+    return neighbors
 
 def getResponse(neighbors):
-	classVotes = {}
-	for x in range(len(neighbors)):
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-	sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
-	return sortedVotes[0][0]
+    classVotes = {}
+    for x in range(len(neighbors)):
+            response = neighbors[x][-1]
+            if response in classVotes:
+                    classVotes[response] += 1
+            else:
+                    classVotes[response] = 1
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
 
 def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
+    correct = 0
+    for x in range(len(testSet)):
+            if testSet[x][-1] == predictions[x]:
+                    correct += 1
+    return (correct/float(len(testSet))) * 100.0
 	
 def main():
 	# prepare data
-	trainingSet=[]
-	testSet=[]
-	split = 0.67
-	loadDataset('iris.data', split, trainingSet, testSet)
-	print('Train set: ' + repr(len(trainingSet)))
-	print('Test set: ' + repr(len(testSet)))
-	# generate predictions
-	predictions=[]
-	k = 3
-	for x in range(len(testSet)):
-		neighbors = getNeighbors(trainingSet, testSet[x], k)
-		result = getResponse(neighbors)
-		predictions.append(result)
-		print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-	accuracy = getAccuracy(testSet, predictions)
-	print('Accuracy: ' + repr(accuracy) + '%')
+    trainingSet=[]
+    testSet=[]
+    split = 0.67
+    loadDataset('data.pickle', split, trainingSet, testSet)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
+    # generate predictions
+    predictions = []
+    k = 6
+    for x in range(len(testSet)):
+        neighbors = getNeighbors(trainingSet, testSet[x], k)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
+    accuracy = getAccuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
 
-def checking_edge_detector():
-    im = Image.new("1", (32,32))
-    draw = ImageDraw.Draw(im)
-    draw.ellipse(((5,5),(25,30)), fill= "white")
-    pix = numpy.array(im)
-    edges2 = canny(pix)
-    print(edges2)
-	
-# main()
-checking_edge_detector()
+main()
