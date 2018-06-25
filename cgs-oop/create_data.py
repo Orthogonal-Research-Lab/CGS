d4c42107cc03b54310af77eca04a4b9a5227fc05
Sam Felder <samfelder17@gmail.com>
2018-06-25 14:36:54 -0500

    fixed hexagon bug

--- cgs-oop/create_data.py
+++ cgs-oop/create_data.py
@@ -33,31 +33,33 @@ def create_data(pixels):
                     if i > 3 and j > 3:
                         draw_rect.rectangle(((i,j),(x+i,y+j)), fill= "white")
                         draw_circle.ellipse(((i,j),(x+i,y+j)), fill= "white")
-                        draw_hex.polygon([(i,j),(i+x,y-j),(i+x+x,y-j),(i+(3*x),j),(i+x+x,y+j),(i+x,y+j)], fill="white")
+                        if( j-1 > y-j):
+                            draw_hex.polygon([(i,j),(i+x,y-j),(i+x+x,y-j),(i+(3*x),j),(i+x+x,y+j),(i+x,y+j)], fill="white")
+                            pix_hex = numpy.array(im_hex)
+                            edges_hex = canny(pix_hex)
+                            list_of_training_data.append((edges_hex,'Hexagon'))
 
                         h = abs(j-y)
                         if(j != y-j and h >2 and h-(y-h) >= 0):
-                            # print("({0},{1}),({2},{3}),({4},{5}),({6},{7}),({8},{9}),({10},{11}),({12},{13}),({14},{15})".format(x,y , x,h, i+x,h-(y-h), x+i+i,h-(y-h), x+(3*i),h ,x+(3*i),y, x+i+i,y+j, i+x,y+j))
                             draw_oct.polygon([(x,y),(x,h),(i+x,h-(y-h)),(x+i+i,h-(y-h)), (x+(3*i),h),(x+(3*i),y),(x+i+i,y+j),(i+x,y+j)], fill="white")
+                            pix_oct = numpy.array(im_oct)
+                            edges_oct = canny(pix_oct)
+                            list_of_training_data.append((edges_oct,'Octagon'))
 
-                        pix_hex = numpy.array(im_hex)
                         pix_rect = numpy.array(im_rect)
                         pix_circle = numpy.array(im_circle)
-                        pix_oct = numpy.array(im_oct)
 
                         # algorithm to extract edges from picture
-                        edges_hex = canny(pix_hex)
                         edges_rect = canny(pix_rect)
                         edges_circle = canny(pix_circle)
-                        edges_oct = canny(pix_oct)
 
                         list_of_training_data.append((edges_rect,'Rectangle'))
                         list_of_training_data.append((edges_circle,'Circle'))
-                        list_of_training_data.append((edges_hex,'Hexagon'))
-                        list_of_training_data.append((edges_oct,'Octagon'))
 
     return list_of_training_data
 
+# print("({0},{1}),({2},{3}),({4},{5}),({6},{7}),({8},{9}),({10},{11}),({12},{13}),({14},{15})".format(x,y , x,h, i+x,h-(y-h), x+i+i,h-(y-h), x+(3*i),h ,x+(3*i),y, x+i+i,y+j, i+x,y+j))
+# print("({0},{1}),({2},{3}),({4},{5}),({6},{7}),({8},{9}),({10},{11})".format(i,j,i+x,y-j,i+x+x,y-j,i+(3*x),j,i+x+x,y+j,i+x,y+j))
 
 # def benchmark_data():
 
