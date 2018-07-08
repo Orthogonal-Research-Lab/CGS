7a2cda331fe3ee92e32e2af533a16e2c31fa20ea
Sam Felder <samfelder17@gmail.com>
2018-07-08 11:09:49 -0500

    created benchmark data for before predictions

--- cgs-oop/create_data.py
+++ cgs-oop/create_data.py
@@ -6,6 +6,9 @@ import random
 import argparse
 from skimage.feature import canny
 import time
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def create_data(pixels):
@@ -37,14 +40,14 @@ def create_data(pixels):
                             draw_hex.polygon([(i,j),(i+x,y-j),(i+x+x,y-j),(i+(3*x),j),(i+x+x,y+j),(i+x,y+j)], fill="white")
                             pix_hex = numpy.array(im_hex)
                             edges_hex = canny(pix_hex)
-                            list_of_training_data.append((edges_hex,'Hexagon'))
+                            list_of_training_data.append((edges_hex,'Hexagon',random.random()))
 
                         h = abs(j-y)
                         if(j != y-j and h >2 and h-(y-h) >= 0):
                             draw_oct.polygon([(x,y),(x,h),(i+x,h-(y-h)),(x+i+i,h-(y-h)), (x+(3*i),h),(x+(3*i),y),(x+i+i,y+j),(i+x,y+j)], fill="white")
                             pix_oct = numpy.array(im_oct)
                             edges_oct = canny(pix_oct)
-                            list_of_training_data.append((edges_oct,'Octagon'))
+                            list_of_training_data.append((edges_oct,'Octagon',random.random()))
 
                         pix_rect = numpy.array(im_rect)
                         pix_circle = numpy.array(im_circle)
@@ -53,15 +56,30 @@ def create_data(pixels):
                         edges_rect = canny(pix_rect)
                         edges_circle = canny(pix_circle)
 
-                        list_of_training_data.append((edges_rect,'Rectangle'))
-                        list_of_training_data.append((edges_circle,'Circle'))
+                        list_of_training_data.append((edges_rect,'Rectangle',random.random()))
+                        list_of_training_data.append((edges_circle,'Circle',random.random()))
 
     return list_of_training_data
 
 # print("({0},{1}),({2},{3}),({4},{5}),({6},{7}),({8},{9}),({10},{11}),({12},{13}),({14},{15})".format(x,y , x,h, i+x,h-(y-h), x+i+i,h-(y-h), x+(3*i),h ,x+(3*i),y, x+i+i,y+j, i+x,y+j))
 # print("({0},{1}),({2},{3}),({4},{5}),({6},{7}),({8},{9}),({10},{11})".format(i,j,i+x,y-j,i+x+x,y-j,i+(3*x),j,i+x+x,y+j,i+x,y+j))
 
-# def benchmark_data():
+def benchmark_data(plot_data):
+
+    fig = plt.figure()
+    N=100
+    ax = fig.add_subplot(1, 1, 1)
+    data  = np.random.random((N, 7))
+
+    x = [data_point[1] for data_point in plot_data]
+    y =[each_color[2] for each_color in plot_data]
+
+    points = data[:,2:4]
+# color is the length of each vector in `points`
+    color = np.sqrt((points**2).sum(axis = 1))/np.sqrt(2.0)
+    rgb = plt.get_cmap('summer')(color)
+    ax.scatter(x, y, color = rgb)
+    plt.show()
 
 if __name__=='__main__':
 
@@ -72,8 +90,10 @@ if __name__=='__main__':
     arg = parser.parse_args()
 
     dataset = create_data(int(arg.pixels))
+# 
+    # with open('data.pickle','wb') as f:
+        # pickle.dump(dataset,f)
 
-    with open('data.pickle','wb') as f:
-        pickle.dump(dataset,f)
+    benchmark_data(dataset)
 
-    print("script finished in {} seconds".format(time.time() - script_start))
+print("script finished in {} seconds".format(time.time() - script_start))
